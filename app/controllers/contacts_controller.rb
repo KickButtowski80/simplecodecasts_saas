47c76894d5404293e688b10bf18b77704edb4bd3
Izak Tarashandegan <pazpaz25@gmail.com>
2016-04-21 03:29:13 +0000

    added contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,8 @@
+class Contacts_Controller < ApplicationController 
+    def new
+    end
+    
+    def create 
+    end
+    
+end
--- db/migrate/20160421031520_create_contacts.rb
+++ db/migrate/20160421031520_create_contacts.rb
@@ -1,6 +1,11 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-    end
+      t.string :name
+      t.string :email
+      t.text   :comments
+      
+      t.timestamps
+     end
   end
 end
