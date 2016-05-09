37c6456d9ddd11e44e5c657df9aaf7dc9a405668
Izak Tarashandegan <pazpaz25@gmail.com>
2016-05-09 20:47:10 +0000

    Added notice and styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :contacts
-  get '/about' => 'pages#about'
-  root 'pages#home'
+  resources :contacts 
+  root "pages#home"
+  get 'about' => 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
