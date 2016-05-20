39e34264f4ea6b85d6eed5faecf60675e6eba244
Izak Tarashandegan <pazpaz25@gmail.com>
2016-05-20 02:41:20 +0000

    put max lenght for first and last name

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,11 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.jpg"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, 
+                    :default_url =>ActionController::Base.helpers.image_path("images/missing_thumb.png")
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
+  
+ #ActionController::Base.helpers.image_path("/missing.png")
  
 end
